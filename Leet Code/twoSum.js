04052acfb8109e31b50ba4515ab7068a7fc10c12
Irfan Ansari <irfan398649@gmail.com>
2023-01-26 09:42:56 +0000

    moveZeroesToBegining [Array partition-  cloud boundary ex]

--- Leet Code/twoSum.js
+++ Leet Code/twoSum.js
@@ -2,5 +2,37 @@
 
 // Two sum 
 
-const arr = [2, 3, 1, 12, 0, 5, 3, 13, -1, -1, -1]
+const arr = [2, 3, 1, 12, 0, 5, 3, 13,-1,-1,-1]
 
+const target = 17
+
+// ask is array sorted if yes then proceed
+const twoSum = (arr, target) => {
+  // sort numbers
+  const compareFunction = (a, b) => {
+    return a - b
+  }
+  arr.sort(compareFunction)
+  console.log('Arr-',arr)
+
+
+  let start = 0
+  let end = arr.length - 1
+
+  while (start < end) {
+    let sum = arr[start] + arr[end]
+    if (sum < target) {
+      start += 1
+    } else if (sum > target) {
+      end -= 1
+    } else {
+      console.log([arr[start], arr[end]])
+      console.log(start, end)
+      return
+    }
+  }
+
+  console.log(-1)
+}
+
+twoSum(arr, target)
--- moveZerosToBegining.js
+++ moveZerosToBegining.js
@@ -2,17 +2,17 @@
 
 // Move all the zeros to the begining of the array
 
-const arr = [2, 0, 9, 12, 0, 25,0,0,89, 39, 13,0,11]
+const arr = [2, 0, 9, 12, 0, 25, 0, 0, 89, 39, 13, 0, 11]
 
-const moveZerosToBegining = (arr)=>{
+const moveZerosToBegining = (arr) => {
   let boundary = 0
-  for(let i=0 ; i<arr.length ; i++){
-    if(arr[i] === 0){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
       // let temp = arr[i]
       // arr[i] = arr[boundary]
       // arr[boundary] = temp
       [arr[i], arr[boundary]] = [arr[boundary], arr[i]]
-       // console.log(arr)
+      // console.log(arr)
       boundary += 1
     }
   }
