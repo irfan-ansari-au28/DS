fac58bde33c2a1a09675f4acffb4b500104f8dc2
Irfan Ansari <irfan398649@gmail.com>
2023-01-26 10:09:12 +0000

    moveZeroesToEnd

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // Array Partion - cloud boundary concept
 
-// Move all the zeros to the begining of the array
+// Move all the zeros to the end of the array
 
 const arr = [2, 0, 9, 12, 0, 25,0,0,89, 39, 13,0,11]
 
-const moveZerosToBegining = (arr)=>{
-  let boundary = 0
-  for(let i=0 ; i<arr.length ; i++){
+const moveZerosToEnd = (arr)=>{
+  let boundary = arr.length - 1
+  let i = arr.length - 1
+  while(i >= 0){
     if(arr[i] === 0){
-      // let temp = arr[i]
-      // arr[i] = arr[boundary]
-      // arr[boundary] = temp
+      // swap number with boundary
       [arr[i], arr[boundary]] = [arr[boundary], arr[i]]
-       // console.log(arr)
-      boundary += 1
+      boundary -= 1
+      console.log(arr)
     }
+    i -= 1
   }
+  
 }
 
-moveZerosToBegining(arr)
+moveZerosToEnd(arr)
